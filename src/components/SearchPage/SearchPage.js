165d87baeb4f079d2d45fa7d0f053a2f0de7dd89
anderson-cahet <anderson.cahet@ghe.megaleo.com>
2020-09-24 13:44:53 +0100

    SearchPage-P01
    type: feature
    - add divs
    - add search bar
     - back button functionality
    - add books interface

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import BooksInterface from '../BookInterface/BooksInterface';
 
 export default class SearchPage extends Component {
-    render() {
-        return (
-            <div className='search-books'>
-                <div className='search-books-bar'>
-                    <Link style={{ cursor: 'pointer' }} className="close-search" to="/" />
-                </div>
-            </div>
-        )
-    }
+	state = {
+        query: '',
+        queryResults:[]
+	};
+	render() {
+        const { query, queryResults } = this.state;
+        
+		return (
+			<div className="search-books">
+				<div className="search-books-bar">
+					<Link style={{ cursor: 'pointer' }} className="close-search" to="/" />
+					<div className="search-books-input-wrapper">
+						<input
+							type="text"
+							placeholder="Search by title or author"
+							value={query}
+							autoFocus={true}
+							onChange={(e) => this.updateQuery(e.target.value)}
+						/>
+					</div>
+				</div>
+				<div className="search-books-results">
+					{
+						<span>
+							Add functionality to show number of books filtered
+							{/* Now showing {filterBooks.length} of {queryResults.length} Books */}
+						</span>
+					}
+                    <ol className='books-grid'>
+                        <BooksInterface />
+                    </ol>
+				</div>
+			</div>
+		);
+	}
 }
